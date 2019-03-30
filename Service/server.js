2029ea1b40a24e79fb7a11e9377a4ede0ac65c37
happy85041vs <happy85041@hotmail.com>
2019-03-30 11:51:38 +0530

    Add service project

---
+++ Service/server.js
@@ -0,0 +1,52 @@
+// server.js
+
+
+var http = require("http");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Chatkit = require('@pusher/chatkit-server');
+
+const app = express();
+
+const chatkit = new Chatkit.default({
+  instanceLocator:'v1:us1:f5a23db4-810f-4bed-8737-f6d3b3d7c67f',
+  key:'1507dbaf-5748-4ed6-81da-07fe54f6c9d8:28nGo8btIkBgMIz0GCnHSgLi/S4vGJ3WatUZK1gOvB4=',
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/users', (req, res) => {
+  const { userId } = req.body;
+  chatkit
+    .createUser({
+      id: userId,
+      name: userId,
+    })
+    .then(() => {
+      res.sendStatus(201);
+    })
+    .catch(err => {
+      if (err.error === 'services/chatkit/user_already_exists') {
+        console.log(`User already exists: ${userId}`);
+        res.sendStatus(200);
+      } else {
+        res.status(err.status).json(err);
+      }
+    });
+});
+
+app.post('/authenticate', (req, res) => {
+  const authData = chatkit.authenticate({
+    userId: req.query.user_id,
+  });
+  res.status(authData.status).send(authData.body);
+});
+
+
+app.set('port', process.env.PORT || 5200);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
